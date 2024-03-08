35392b82851744a9df458fb71bf35273dc0c398e
Pushpendra Baswal <pushpendra9122@gmail.com>
2024-03-08 14:04:12 +0530

    create slot booking page done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:child_care/views/screens/callender_attendence.dart';
 import 'package:child_care/views/screens/child_today_update_page.dart';
 import 'package:child_care/views/screens/homepage.dart';
 import 'package:child_care/views/screens/leave_request.dart';
+import 'package:child_care/views/screens/slot_booking.dart';
 import 'package:child_care/views/screens/students_attendence.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LeaveRequestPage(),
+      home: slotBooking(),
     );
   }
 }
